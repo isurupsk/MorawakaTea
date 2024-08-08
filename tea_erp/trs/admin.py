6a3a8678b80f1725333e31e1e23cdf5fc167b5ab
isurupsk <isurupsk@gmail.com>
2024-08-08 21:54:07 +0530

    ModelsUpdate

--- tea_erp/trs/admin.py
+++ tea_erp/trs/admin.py
@@ -1,57 +1,153 @@
+# from django.contrib import admin
+# from .models import (
+#     Bank, Supplier, Root, Officer, Supervision,
+#     DailyGreenLeaf, SupplierPayment, Loan,
+#     AdvancePayment, LoanRepayment, RootTransportAgent, RootTransport
+# )
+
+# class BankAdmin(admin.ModelAdmin):
+#     list_display = ('bank_id', 'bank_name', 'bank_branch', 'account_number')
+
+# class SupplierAdmin(admin.ModelAdmin):
+#     list_display = ('supplier_id', 'record_id', 'supplier_name', 'date', 'contact_info', 'address', 'bank')
+
+# class RootAdmin(admin.ModelAdmin):
+#     list_display = ('root_id', 'root_name', 'description')
+
+# class OfficerAdmin(admin.ModelAdmin):
+#     list_display = ('officer_id', 'name')
+
+# class SupervisionAdmin(admin.ModelAdmin):
+#     list_display = ('supervision_id', 'name')
+
+# class DailyGreenLeafAdmin(admin.ModelAdmin):
+#     list_display = ('daily_green_leaf_id', 'supplier', 'root', 'date', 'full_quantity', 'leaves_quantity', 
+#                     'gold_leaves', 'empty_cases', 'water', 'matured_leaves', 'boiled_leaves', 
+#                     'good_leaves', 'supervision', 'officer')
+
+# class SupplierPaymentAdmin(admin.ModelAdmin):
+#     list_display = ('supplier_payment_id', 'supplier', 'root', 'transaction_date', 'quantity', 'total_amount')
+
+# class LoanAdmin(admin.ModelAdmin):
+#     list_display = ('loan_id', 'supplier', 'date', 'loan_amount', 'root')
+
+# class AdvancePaymentAdmin(admin.ModelAdmin):
+#     list_display = ('advance_payment_id', 'supplier', 'date', 'advance_amount', 'root')
+
+# class LoanRepaymentAdmin(admin.ModelAdmin):
+#     list_display = ('loan_repayment_id', 'supplier', 'date', 'amount', 'root', 'loan', 'balance')
+
+# class RootTransportAgentAdmin(admin.ModelAdmin):
+#     list_display = ('root_transport_agent_id', 'supplier', 'agent_name')
+
+# class RootTransportAdmin(admin.ModelAdmin):
+#     list_display = ('root_transport_id', 'root', 'transport_agent')
+
+# admin.site.register(Bank, BankAdmin)
+# admin.site.register(Supplier, SupplierAdmin)
+# admin.site.register(Root, RootAdmin)
+# admin.site.register(Officer, OfficerAdmin)
+# admin.site.register(Supervision, SupervisionAdmin)
+# admin.site.register(DailyGreenLeaf, DailyGreenLeafAdmin)
+# admin.site.register(SupplierPayment, SupplierPaymentAdmin)
+# admin.site.register(Loan, LoanAdmin)
+# admin.site.register(AdvancePayment, AdvancePaymentAdmin)
+# admin.site.register(LoanRepayment, LoanRepaymentAdmin)
+# admin.site.register(RootTransportAgent, RootTransportAgentAdmin)
+# admin.site.register(RootTransport, RootTransportAdmin)
+
+
+
+
 from django.contrib import admin
 from .models import (
-    Bank, Supplier, Root, Officer, Supervision,
-    DailyGreenLeaf, SupplierPayment, Loan,
-    AdvancePayment, LoanRepayment, RootTransportAgent, RootTransport
+    UserRole, User, Bank, Supplier, Root, Officer, Supervision, 
+    RootTransportAgent, DailyGreenLeaf, SupplierPayment, Loan, 
+    AdvancePayment, LoanRepayment, RootTransport, PaymentMode
 )
 
+@admin.register(UserRole)
+class UserRoleAdmin(admin.ModelAdmin):
+    list_display = ('user_role_id', 'user_role', 'status')
+    search_fields = ('user_role',)
+    list_filter = ('status',)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'user_name', 'status', 'created_date', 'user_role')
+    search_fields = ('user_name',)
+    list_filter = ('status', 'user_role')
+
+@admin.register(Bank)
 class BankAdmin(admin.ModelAdmin):
     list_display = ('bank_id', 'bank_name', 'bank_branch', 'account_number')
+    search_fields = ('bank_name', 'bank_branch')
 
+@admin.register(Supplier)
 class SupplierAdmin(admin.ModelAdmin):
-    list_display = ('supplier_id', 'record_id', 'supplier_name', 'date', 'contact_info', 'address', 'bank')
+    list_display = ('supplier_id', 'supplier_name', 'contact_info', 'address', 'land_size', 'bank')
+    search_fields = ('supplier_name', 'contact_info', 'record_id')
+    list_filter = ('bank',)
 
+@admin.register(Root)
 class RootAdmin(admin.ModelAdmin):
-    list_display = ('root_id', 'root_name', 'description')
+    list_display = ('root_id', 'root_name', 'root_code', 'description')
+    search_fields = ('root_name', 'root_code')
 
+@admin.register(Officer)
 class OfficerAdmin(admin.ModelAdmin):
-    list_display = ('officer_id', 'name')
+    list_display = ('officer_id', 'name', 'user')
+    search_fields = ('name',)
+    list_filter = ('user',)
 
+@admin.register(Supervision)
 class SupervisionAdmin(admin.ModelAdmin):
-    list_display = ('supervision_id', 'name')
+    list_display = ('supervision_id', 'name', 'user')
+    search_fields = ('name',)
+    list_filter = ('user',)
 
-class DailyGreenLeafAdmin(admin.ModelAdmin):
-    list_display = ('daily_green_leaf_id', 'supplier', 'root', 'date', 'full_quantity', 'leaves_quantity', 
-                    'gold_leaves', 'empty_cases', 'water', 'matured_leaves', 'boiled_leaves', 
-                    'good_leaves', 'supervision', 'officer')
-
-class SupplierPaymentAdmin(admin.ModelAdmin):
-    list_display = ('supplier_payment_id', 'supplier', 'root', 'transaction_date', 'quantity', 'total_amount')
-
-class LoanAdmin(admin.ModelAdmin):
-    list_display = ('loan_id', 'supplier', 'date', 'loan_amount', 'root')
-
-class AdvancePaymentAdmin(admin.ModelAdmin):
-    list_display = ('advance_payment_id', 'supplier', 'date', 'advance_amount', 'root')
-
-class LoanRepaymentAdmin(admin.ModelAdmin):
-    list_display = ('loan_repayment_id', 'supplier', 'date', 'amount', 'root', 'loan', 'balance')
-
+@admin.register(RootTransportAgent)
 class RootTransportAgentAdmin(admin.ModelAdmin):
-    list_display = ('root_transport_agent_id', 'supplier', 'agent_name')
+    list_display = ('root_transport_agent_id', 'agent_name')
+    search_fields = ('agent_name',)
 
+@admin.register(DailyGreenLeaf)
+class DailyGreenLeafAdmin(admin.ModelAdmin):
+    list_display = ('daily_green_leaf_id', 'supplier', 'root', 'date', 'full_quantity', 'leaves_quantity')
+    search_fields = ('supplier__supplier_name', 'root__root_name')
+    list_filter = ('supplier', 'root', 'date')
+
+@admin.register(SupplierPayment)
+class SupplierPaymentAdmin(admin.ModelAdmin):
+    list_display = ('supplier_payment_id', 'supplier', 'payment_mode', 'transaction_date', 'total_amount', 'cheque_number')
+    search_fields = ('supplier__supplier_name', 'cheque_number')
+    list_filter = ('supplier', 'payment_mode', 'transaction_date')
+
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('loan_id', 'supplier', 'date', 'loan_amount', 'root', 'cheque_number', 'payment_mode')
+    search_fields = ('supplier__supplier_name', 'cheque_number')
+    list_filter = ('supplier', 'root', 'payment_mode', 'date')
+
+@admin.register(AdvancePayment)
+class AdvancePaymentAdmin(admin.ModelAdmin):
+    list_display = ('advance_payment_id', 'supplier', 'date', 'advance_amount', 'root', 'cheque_number', 'payment_mode')
+    search_fields = ('supplier__supplier_name', 'cheque_number')
+    list_filter = ('supplier', 'root', 'payment_mode', 'date')
+
+@admin.register(LoanRepayment)
+class LoanRepaymentAdmin(admin.ModelAdmin):
+    list_display = ('loan_repayment_id', 'supplier', 'date', 'amount', 'balance', 'loan', 'root')
+    search_fields = ('supplier__supplier_name',)
+    list_filter = ('supplier', 'root', 'date')
+
+@admin.register(RootTransport)
 class RootTransportAdmin(admin.ModelAdmin):
     list_display = ('root_transport_id', 'root', 'transport_agent')
+    search_fields = ('root__root_name', 'transport_agent__agent_name')
+    list_filter = ('root', 'transport_agent')
 
-admin.site.register(Bank, BankAdmin)
-admin.site.register(Supplier, SupplierAdmin)
-admin.site.register(Root, RootAdmin)
-admin.site.register(Officer, OfficerAdmin)
-admin.site.register(Supervision, SupervisionAdmin)
-admin.site.register(DailyGreenLeaf, DailyGreenLeafAdmin)
-admin.site.register(SupplierPayment, SupplierPaymentAdmin)
-admin.site.register(Loan, LoanAdmin)
-admin.site.register(AdvancePayment, AdvancePaymentAdmin)
-admin.site.register(LoanRepayment, LoanRepaymentAdmin)
-admin.site.register(RootTransportAgent, RootTransportAgentAdmin)
-admin.site.register(RootTransport, RootTransportAdmin)
+@admin.register(PaymentMode)
+class PaymentModeAdmin(admin.ModelAdmin):
+    list_display = ('payment_mode_id', 'payment_mode')
+    search_fields = ('payment_mode',)
--- tea_erp/trs/models.py
+++ tea_erp/trs/models.py
@@ -2,6 +2,29 @@
 
 from django.db import models
 
+
+# Users  tables 
+
+class UserRole(models.Model):
+    user_role_id = models.AutoField(primary_key=True)
+    user_role = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user_role
+    
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    status = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    user_role = models.ForeignKey(UserRole, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_name
+
+
 class Bank(models.Model):
     bank_id = models.AutoField(primary_key=True)
     bank_name = models.CharField(max_length=255)
@@ -18,6 +41,7 @@ class Supplier(models.Model):
     date = models.DateField() 
     contact_info = models.CharField(max_length=255)
     address = models.TextField()
+    land_size = models.DecimalField(max_digits=10, decimal_places=2) 
     bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -26,6 +50,7 @@ class Supplier(models.Model):
 class Root(models.Model):
     root_id = models.AutoField(primary_key=True)
     root_name = models.CharField(max_length=255)
+    root_code = models.CharField(max_length=50)
     description = models.TextField()
 
     def __str__(self):
@@ -34,6 +59,7 @@ class Root(models.Model):
 class Officer(models.Model):
     officer_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -41,9 +67,18 @@ class Officer(models.Model):
 class Supervision(models.Model):
     supervision_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+    
+class RootTransportAgent(models.Model):
+    root_transport_agent_id = models.AutoField(primary_key=True)
+
+    agent_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Transport Agent {self.agent_name}'
 
 class DailyGreenLeaf(models.Model):
     daily_green_leaf_id = models.AutoField(primary_key=True)
@@ -60,6 +95,7 @@ class DailyGreenLeaf(models.Model):
     good_leaves = models.DecimalField(max_digits=10, decimal_places=2)
     supervision = models.ForeignKey(Supervision, on_delete=models.CASCADE)
     officer = models.ForeignKey(Officer, on_delete=models.CASCADE)
+    root_transport_agent = models.ForeignKey(RootTransportAgent, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Daily Green Leaf {self.daily_green_leaf_id}'
@@ -96,6 +132,61 @@ class AdvancePayment(models.Model):
     def __str__(self):
         return f'Advance Payment {self.advance_payment_id}'
 
+
+
+
+
+class RootTransport(models.Model):
+    root_transport_id = models.AutoField(primary_key=True)
+    root = models.ForeignKey(Root, on_delete=models.CASCADE)
+    transport_agent = models.ForeignKey(RootTransportAgent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Root Transport {self.root_transport_id}'
+
+class PaymentMode(models.Model):
+    payment_mode_id = models.AutoField(primary_key=True)
+    payment_mode = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.payment_mode
+
+class SupplierPayment(models.Model):
+    supplier_payment_id = models.AutoField(primary_key=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    payment_mode = models.ForeignKey(PaymentMode, on_delete=models.CASCADE)
+    transaction_date = models.DateTimeField()
+    quantity = models.IntegerField()
+    cheque_number = models.CharField(max_length=50, null=True, blank=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Transaction {self.supplier_payment_id}'
+    
+class Loan(models.Model):
+    loan_id = models.AutoField(primary_key=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    date = models.DateField()
+    loan_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    root = models.ForeignKey(Root, on_delete=models.CASCADE)
+    cheque_number = models.CharField(max_length=50, null=True, blank=True)
+    payment_mode = models.ForeignKey(PaymentMode, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Loan {self.loan_id}'
+
+class AdvancePayment(models.Model):
+    advance_payment_id = models.AutoField(primary_key=True)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    date = models.DateField()
+    advance_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    root = models.ForeignKey(Root, on_delete=models.CASCADE)
+    cheque_number = models.CharField(max_length=50, null=True, blank=True)
+    payment_mode = models.ForeignKey(PaymentMode, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Advance Payment {self.advance_payment_id}'
+    
 class LoanRepayment(models.Model):
     loan_repayment_id = models.AutoField(primary_key=True)
     supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
@@ -108,19 +199,4 @@ class LoanRepayment(models.Model):
     def __str__(self):
         return f'Loan Repayment {self.loan_repayment_id}'
 
-class RootTransportAgent(models.Model):
-    root_transport_agent_id = models.AutoField(primary_key=True)
-    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
-    agent_name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f'Transport Agent {self.agent_name}'
-
-class RootTransport(models.Model):
-    root_transport_id = models.AutoField(primary_key=True)
-    root = models.ForeignKey(Root, on_delete=models.CASCADE)
-    transport_agent = models.ForeignKey(RootTransportAgent, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f'Root Transport {self.root_transport_id}'
 
